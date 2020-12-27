20be00b6ec3b3cc8687af9c2bba89a2b507d5b5a
khardrix <carmenandryanhuffman@yahoo.com>
2020-12-27 01:39:45 -0500

    working on getting snapShot and componentDidMount() function done in App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,21 @@ class App extends React.Component {
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             // createUserProfileDocument(user);
             if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
 
+                userRef.onSnapshot(snapShot => {
+                    // console.log(snapShot.data());
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    })
+                });
+                console.log(this.state);
+            }
+            else {
+                this.setState({currentUser: userAuth});
             }
         });
     }
