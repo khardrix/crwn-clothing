b87b7d95ac883fe70e3a81db7b31841ed43718d1
khardrix <carmenandryanhuffman@yahoo.com>
2020-12-28 04:49:05 -0500

    fixing console.log inside of componentDidMount(){...} inside of App.js

--- src/App.js
+++ src/App.js
@@ -24,20 +24,19 @@ class App extends React.Component {
 
     componentDidMount() {
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-            // createUserProfileDocument(user);
             if (userAuth) {
                 const userRef = await createUserProfileDocument(userAuth);
 
                 userRef.onSnapshot(snapShot => {
-                    // console.log(snapShot.data());
                     this.setState({
                         currentUser: {
                             id: snapShot.id,
                             ...snapShot.data()
                         }
-                    })
+                    }, () => {
+                        console.log(this.state);
+                    });
                 });
-                console.log(this.state);
             } 
             else {
                 this.setState({currentUser: userAuth});
