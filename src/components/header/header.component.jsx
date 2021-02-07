1e4068faaf7d9c909b14ebc5e0fc4915e65177cc
khardrix <carmenandryanhuffman@yahoo.com>
2021-02-07 00:02:24 -0500

    lesson 114 finished

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
+
 
 const Header = ({ currentUser }) => (
     <div className='header'>
@@ -30,6 +32,10 @@ const Header = ({ currentUser }) => (
             }
         </div>
     </div>
-)
+);
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
