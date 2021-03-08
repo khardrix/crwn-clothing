72ddfedece3ae674d84904632f1a4954121afa3b
khardrix <carmenandryanhuffman@yahoo.com>
2021-03-07 22:48:35 -0500

    added shopping cart icon to right-hand corner of the header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
-
 
 const Header = ({ currentUser }) => (
     <div className='header'>
@@ -30,6 +31,7 @@ const Header = ({ currentUser }) => (
                     SIGN IN
                 </Link>
             }
+            <CartIcon />
         </div>
     </div>
 );
