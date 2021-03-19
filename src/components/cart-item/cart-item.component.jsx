2dafec56e02d9292df67b49febd5c178e41bd70d
khardrix <carmenandryanhuffman@yahoo.com>
2021-03-19 17:50:06 -0400

    added none_modules

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -4,6 +4,12 @@ import './cart-item.styles.scss';
 
 const CartItem = ({ item: { imageUrl, price, name } }) => (
     <div className='cart-item'>
-    
+        <img src={imageUrl} alt='item' />
+        <div className='item-details'>
+            <span className='name'>{name}</span>
+            <span className='price'>${price}</span>
+        </div>
     </div>
-)
+);
+
+export default CartItem;
